853aa0ee61670d98d68b8e36922603588637d19d
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-03-07 22:22:11 -0600

    started methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,22 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+    }
+
+    public static int addition(int num1, int numb2) {
+        return num1 + numb2;
+    }
+
+    public static int subtract(int num1, int numb2) {
+        return num1 - numb2;
+    }
+
+    public static int multiply(int num1, int numb2) {
+        return num1 * numb2;
+    }
+
+    public static int divide(int num1, int numb2) {
+        return num1 / numb2;
+    }
+
+}
