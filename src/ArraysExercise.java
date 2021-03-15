d5bb40abdbb3de84a3412de7056ea94e2f3bd46d
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-03-14 20:01:30 -0500

    Arrays and Generator exs completed

--- src/ArraysExercise.java
+++ src/ArraysExercise.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 
 public class ArraysExercise {
@@ -5,16 +6,31 @@ public class ArraysExercise {
        int[] numbers = {1, 2, 3, 4, 5};
        System.out.println(Arrays.toString(numbers));
 
-       Person[] persons = new Person[1];
+       Person[] people = new Person[1];
 
 
-       Person p1 = new Person ("Jacob");
-       Person p2 = new Person ("Edward");
-       Person p3 = new Person ("Patrick");
+       people [0] = new Person ("Jacob");
+       people [1] = new Person ("Edward");
+       people [2] = new Person ("Patrick");
+
+
+       for (int i = 0; i < people.length; i++) {
+           System.out.println(people[i].getName());
+       }
+
+       people = addPerson(people, new Person("Plankton"));
+
+       for(Person person : people) {
+           System.out.println("Using enhanced for: " + person.getName());
+       }
 
     }
+        public static  Person[] addPerson(Person[] people, Person person)
+        {
+            Person[] newPeopleArray = Arrays.copyOf(people, people.length + 1);
 
+            newPeopleArray[newPeopleArray.length - 1] = person;
 
-
-
+            return newPeopleArray;
+        }
 }
