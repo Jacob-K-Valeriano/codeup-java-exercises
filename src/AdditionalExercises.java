00417c8000cce9c11545c6abb7a45a48d496f430
Jacob-Valeriano <jacob.k.valeriano@gmail.com>
2021-04-23 17:07:13 -0500

    additional math practice exercises

---
+++ src/AdditionalExercises.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class AdditionalExercises {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input an integer number: ");
+        int x = in.nextInt();
+        System.out.printf("The absolute value of %d is: %d", x, convert(x));
+        System.out.printf("\n");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = scan.nextInt();
+        if (num > 0)
+            System.out.println("Count the numbers without digit 7, from 1 to " + num + ": " + count_nums_not_7(num));
+    }
+
+    private static void count_nums_not_7(int num) {
+    }
+
+    public static int convert(int n) {
+        int absvalue = (n >= 0) ? n : -n;
+        return absvalue;
+    }
+
+    class solution {
+        int count_nums_not_7(int num) {
+            if (num < 7)
+                return num;
+            if (num >= 7 && num < 10)
+                return num - 1;
+
+            int r = 1;
+            while (num / r > 9)
+                r = r * 10;
+
+            int m = num / r;
+
+            if (m != 7)
+                return count_nums_not_7(m) * count_nums_not_7(r - 1) + count_nums_not_7(m) + count_nums_not_7(num % r);
+            else
+                return count_nums_not_7(m * r - 1);
+        }
+    }
+}
